2ba24eaad834afbbfd5cabcbfc7cf318087bf62b
khad <khadidjamehdi@hotmail.com>
2020-10-09 16:37:57 +0200

    nouvelle classe déplacement

--- src/action/Déplacement.java
+++ src/action/Déplacement.java
@@ -10,17 +10,17 @@ import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
 
 public class Déplacement {
-	
+
 	public Déplacement() {
 		Wheel leftWheel = WheeledChassis.modelWheel(Motor.B,0.056).offset(-0.06075); // 1.5 = diam�tre des roues, offset = d�calage des roues ??
 		Wheel rightWheel = WheeledChassis.modelWheel(Motor.C, 0.056).offset(0.06075);
 		Chassis chassis = new WheeledChassis(new Wheel[] {leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
 		pilot = new MovePilot(chassis);
-		
+
 		pilot.setAngularSpeed(90);
-		
+
 	}
- 
+
 	private MovePilot pilot;
 	int position = 0;
 
@@ -57,38 +57,7 @@ public class Déplacement {
 			position=360+position;
 
 	}
-	
-	// Méthodes qui permettent de repositionner le robot vers les sens prédéfinies
-	/* public void nord() {
-		if (position<=180) 
-			turnLeft(position);
-		else if (position>180)
-			turnRight(360-position);
-	}
 
-	public void sud() {
-		if (position<=180) {
-			turnRight(180-position);
-		}
-		else if (position>180)
-			turnLeft(position-180);
-	}
-	
-	public void est() {
-		if (position<=90) {
-			turnRight(90-position);
-		}
-		else if (position>90)
-			turnLeft(position-90);
-	}
-	
-	public void ouest() {
-		if (position<=270) {
-			turnRight(270-position);
-		}
-		else if (position>270)
-			turnLeft(position-270);
-	*/ }
-	
-	
+}
+
 
