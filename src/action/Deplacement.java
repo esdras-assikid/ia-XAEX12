b1d29317d0406955df9e88760dd54c8f9744c388
khad la boss <khadidjamehdi@hotmail.com>
2020-10-12 13:28:16 +0200

    classe deplacement sans l'accent

---
+++ src/action/Deplacement.java
@@ -0,0 +1,63 @@
+package action;
+
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+import lejos.utility.Delay;
+
+public class Deplacement {
+
+	public Deplacement() {
+		Wheel leftWheel = WheeledChassis.modelWheel(Motor.B,0.056).offset(-0.06075); // 1.5 = diam�tre des roues, offset = d�calage des roues ??
+		Wheel rightWheel = WheeledChassis.modelWheel(Motor.C, 0.056).offset(0.06075);
+		Chassis chassis = new WheeledChassis(new Wheel[] {leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+		pilot = new MovePilot(chassis);
+
+		pilot.setAngularSpeed(90);
+
+	}
+
+	private MovePilot pilot;
+	int position = 0;
+
+	// MÉTHODES //
+	public void avancer() {
+		pilot.forward();
+	}
+
+	public void reculer() {
+		pilot.backward();
+	}
+
+	public void stop(){
+		pilot.stop();
+	}
+
+	// Rotation dans le sens des aiguilles d'une montre
+
+	public void turnLeft(int angle) {
+		pilot.rotate(-angle);
+		modifierPosition(-angle);
+	}
+
+	public void turnRight(int angle) {
+		pilot.rotate(angle);
+		modifierPosition(angle);
+	}
+
+	public void modifierPosition(int angle) {
+		position+=angle;
+		if (position>=360)
+			position-=360;
+		if (position<0)
+			position=360+position;
+
+	}
+
+}
+
+
