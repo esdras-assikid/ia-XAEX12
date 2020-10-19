093eb1eed0ecc5f693cfbe2a9164ce9e7413e40a
pilopa84 <71647292+pilopa84@users.noreply.github.com>
2020-10-19 16:01:52 +0200

    pierre

--- src/perception/TouchSensor.java
+++ src/perception/TouchSensor.java
@@ -1,29 +1,40 @@
 package perception;
 
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.*;
 
+public class TouchSensor {
 
-public class TouchSensor extends EV3TouchSensor {
-
-	public TouchSensor(Port port)
+	private EV3TouchSensor ts;
+	private SampleProvider source;
+	
+	public TouchSensor()
     {
-        super(port);
+		Brick b = BrickFinder.getDefault();
+		Port s3 = b.getPort("S3");
+		ts = new EV3TouchSensor(s3);
+		source = ts.getTouchMode();
     }
 
     public boolean isPressed()
     {
         float[] sample = new float[1];
-        fetchSample(sample, 0);
-
+        source.fetchSample(sample, 0);
+        
         return sample[0] != 0;
         
     }
     
+    
+    
+    
     public int Count() {
     	
     	float[] sample = new float[1];
-    	fetchSample(sample, 0);
+    	source.fetchSample(sample, 0);
         
     	int i = 0;
     	if (sample[0] != 0) {
